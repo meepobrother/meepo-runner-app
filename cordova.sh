fa4452a00c11c0dc47599ea2e4f95881797dd38f
15138730370 <1037483576@qq.com>
2018-03-30 13:31:13 +0800

    update

--- cordova.sh
+++ cordova.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-ionic cordova plugin add cordova-plugin-background-mode
-ionic cordova plugin add cordova-plugin-add-swift-support
+# ionic cordova plugin add cordova-plugin-background-mode
+# ionic cordova plugin add cordova-plugin-add-swift-support
 
 ionic cordova plugin add cordova-plugin-device
 ionic cordova plugin add cordova-plugin-device-motion
@@ -13,9 +13,9 @@ ionic cordova plugin add cordova-plugin-ionic-webview
 
 ionic cordova plugin add cordova-plugin-local-notification
 
-ionic cordova plugin add cordova-plugin-qqsdk --variable QQ_APP_ID=1106539177
-ionic cordova plugin add cordova-plugin-qrscanner
-ionic cordova plugin add cordova-plugin-shake
+# ionic cordova plugin add cordova-plugin-qqsdk --variable QQ_APP_ID=1106539177
+# ionic cordova plugin add cordova-plugin-qrscanner
+# ionic cordova plugin add cordova-plugin-shake
 
 ionic cordova plugin add cordova-plugin-splashscreen
 ionic cordova plugin add cordova-plugin-statusbar
@@ -25,12 +25,12 @@ ionic cordova plugin add cordova-plugin-tts
 ionic cordova plugin add cordova-plugin-uniquedeviceid
 
 ionic cordova plugin add cordova-plugin-whitelist
-ionic cordova plugin add cordova-plugin-zeroconf
+# ionic cordova plugin add cordova-plugin-zeroconf
 ionic cordova plugin add cordova-sms-plugin
 
-ionic cordova plugin add cordova-sqlite-storage
+# ionic cordova plugin add cordova-sqlite-storage
 
-ionic cordova plugin add cordova-plugin-baidumaplocation --variable ANDROID_KEY="pALGl86Gqboa8s9rd4p6GVuXm1TQ4b2g" --variable IOS_KEY="pALGl86Gqboa8s9rd4p6GVuXm1TQ4b2g"
+# ionic cordova plugin add cordova-plugin-baidumaplocation --variable ANDROID_KEY="pALGl86Gqboa8s9rd4p6GVuXm1TQ4b2g" --variable IOS_KEY="pALGl86Gqboa8s9rd4p6GVuXm1TQ4b2g"
 
 # ionic cordova plugin add cordova-plugin-camera
 # ionic cordova plugin add cordova-plugin-camera-preview
